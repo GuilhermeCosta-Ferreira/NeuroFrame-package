688fd913344f1532481fb7be03efe07ca2ea4163
GuilhermeCF <guicostaferreira.2003@gmail.com>
2025-12-13 11:23:10 +0100

    feat: Added styling module and enhance color utilities
    - Updated `pyproject.toml` to modify numpy version constraint and added new dependencies: simpleitk, scipy, and opencv-python.
    - Refactored `__init__.py` to include new modules: pipeline and plots.
    - Introduced `color_converter.py` with a function to convert hex colors to RGB.
    - Added `colormap_utils.py` for functions to retrieve segment separators and ID divisions.
    - Created `colormaps.py` to define alpha colormaps using the new color utilities.
    - Implemented `colors.py` to store color constants and alpha color definitions.
    - Developed `graphic_classes.py` to define an AlphaColor class for handling colors with transparency.
    - Added `graphic_utils.py` for background removal and colormap utilities, including functions for creating transparent colormaps and displaying them.

--- neuroframe/src/neuroframe/__init__.py
+++ neuroframe/src/neuroframe/__init__.py
@@ -1,3 +1,6 @@
+from .styling import *
 from .utils import *
 from .mouse_data import *
 from .mouse import *
+from .pipeline import *
+from .plots import *
---
+++ neuroframe/src/neuroframe/styling/colormaps.py
@@ -0,0 +1,12 @@
+# ================================================================
+# 0. Section: Imports
+# ================================================================
+from .graphic_utils import transparent_to_color_cmap
+from .colors import *
+
+
+# ================================================================
+# 1. Section: Colormaps
+# ================================================================
+alpha_red_cmap_256 = transparent_to_color_cmap(NR_RED)
+alpha_blue_cmap_256 = transparent_to_color_cmap(NR_BLUE)
