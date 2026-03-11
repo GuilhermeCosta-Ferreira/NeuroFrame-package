22d59211fe67bd8e4b5021ef8074d475460fd425
GuilhermeCF <guicostaferreira.2003@gmail.com>
2026-03-11 13:18:42 +0100

    feat: added the BL alignment space

--- scripts/group_probability_map.py
+++ scripts/group_probability_map.py
@@ -3,6 +3,9 @@
 # ================================================================
 import re
 
+import numpy as np
+import pandas as pd
+
 from pathlib import Path
 from neuroframe import Mouse
 
@@ -10,6 +13,7 @@ from neuroframe.group_analysis import (
     get_average_volume,
     pick_closest_to_average
 )
+from neuroframe.pipeline import layer_colapsing
 
 
 # ================================================================
@@ -18,6 +22,7 @@ from neuroframe.group_analysis import (
 BASE_DIR: Path = Path(__file__).resolve().parents[2]
 MICE_FOLDER: Path = BASE_DIR / "data"
 REFERENCE_MOUSE: Path | None = MICE_FOLDER / "S872"
+SEGMENT_INFO_PATH: Path = BASE_DIR / "data" / "annotations_info.csv"
 
 
 
@@ -28,6 +33,15 @@ def get_mouse_folders(folder: Path) -> list[Path]:
     pattern = re.compile(r"^[A-Z]\d{3}$")
     return sorted([p for p in folder.iterdir() if p.is_dir() and pattern.match(p.name)])
 
+def get_unique_segments(mice: list[Path]) -> np.ndarray:
+    uniques = []
+    for mouse in mice:
+        mouse = Mouse.from_folder(mouse.name, str(mouse), was_analysed=True)
+        labels = mouse.segmentation.labels
+
+        uniques.append(labels)
+
+    return np.unique(np.concatenate(uniques))
 
 # ================================================================
 # 3. Section: MAIN
@@ -40,5 +54,43 @@ if __name__ == '__main__':
     if(REFERENCE_MOUSE is None):
         average_volume = get_average_volume(mice)
         REFERENCE_MOUSE = pick_closest_to_average(mice, average_volume)
+    reference_mouse = Mouse.from_folder(
+        id=REFERENCE_MOUSE.name,
+        folder_path=str(REFERENCE_MOUSE),
+        was_analysed=True
+    )
 
-    # 3.
+    # 3. Instantiates the average mouse
+    probability_map = np.zeros(reference_mouse.data_shape)
+    lateralization_map = np.zeros(reference_mouse.data_shape)
+    segmentation_map = np.zeros(reference_mouse.data_shape)
+    bl_map = reference_mouse.field_bl.data.copy()
+
+    # 4. Gets all unique segments alonga all mice
+    all_segments = get_unique_segments(mice)
+
+    # 5. Gets all BL Translation vectors
+    reference_zero = np.argwhere(np.all(bl_map == [0, 0, 0], axis=-1))[0]
+    for mouse in mice:
+        if(mouse.name == REFERENCE_MOUSE.name):
+            continue
+
+        mouse = Mouse.from_folder(mouse.name, str(mouse), True)
+        bl_space = mouse.field_bl.data
+        hemispheres = mouse.hemisphere.data
+        mouse_nedt = mouse.segmentation_nedt
+        mouse_segments = mouse.segmentation.data
+
+        mouse_zero = np.argwhere(np.all(bl_space == [0, 0, 0], axis=-1))[0]
+
+        translation = reference_zero - mouse_zero
+
+        import numpy as np
+
+        shifted = np.roll(mouse_segments, shift=tuple(translation), axis=(0, 1, 2))
+        break
+
+
+        # 5.1
+
+    # 6. Loops over every segment
--- src/neuroframe/mouse/Mouse.py
+++ src/neuroframe/mouse/Mouse.py
@@ -57,24 +57,49 @@ class Mouse(Dunders, Properties, Plots):
         self.id = id
 
     @classmethod
-    def from_folder(cls, id: str, folder_path: str) -> 'Mouse':
-        # Makes sure is safe to proceed
+    def from_folder(cls, id: str, folder_path: str, was_analysed: bool = False) -> 'Mouse':
+        # 1. Makes sure is safe to proceed
         assert_required_files(folder_path)
         assert_no_extra_files(folder_path)
 
+        # 2. Extracts the files in the folder
         files = os.listdir(folder_path)
-        target_files = ['_mri', '_uCT', '_seg']
+        if not was_analysed:
+            target_files = ['_mri', '_uCT', '_seg']
+        else:
+            target_files = ['_proc_mri', '_proc_ct', '_proc_seg']
 
+        # 3. Assigns the files to the correct place
         for target in target_files:
             target_file = [file for file in files if target in file][0]
 
             file_path = os.path.join(folder_path, target_file)
 
-            if target == '_mri': mri_path = file_path
-            elif target == '_uCT': ct_path = file_path
-            elif target == '_seg': segmentations_path = file_path
+            if target == target_files[0]: mri_path = file_path
+            elif target == target_files[1]: ct_path = file_path
+            elif target == target_files[2]: segmentations_path = file_path
 
-        return cls(id, mri_path, ct_path, segmentations_path)
+        # 4. Starts the mouse
+        mouse = cls(id, mri_path, ct_path, segmentations_path)
+
+        # 5. If it has been processed then we can add
+        if was_analysed:
+            target_files = ['_bl_space', '_edt', '_nedt', '_sides.']
+
+            for target in target_files:
+                target_file = [file for file in files if target in file][0]
+                file_path = os.path.join(folder_path, target_file)
+
+                if target == target_files[0]:
+                    mouse.add_path(file_path, FieldBL)
+                elif target == target_files[1]:
+                    mouse.add_path(file_path, SegmentationEDT)
+                elif target == target_files[2]:
+                    mouse.add_path(file_path, SegmentationNEDT)
+                elif target == target_files[3]:
+                    mouse.add_path(file_path, Hemisphere)
+
+        return mouse
 
 
 
